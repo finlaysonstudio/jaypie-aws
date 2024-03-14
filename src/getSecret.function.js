6c2a30181903dd8f1e3208f20b7b862cbcef679e
Adam Finlayson <amf@adamfinlayson.com>
2024-03-13 22:40:29 -0500

    feat: init: magpie get secret function

---
+++ src/getSecret.function.js
@@ -0,0 +1,55 @@
+import { ConfigurationError, log } from "@jaypie/core";
+
+import axios from "axios";
+
+//
+//
+// Constants
+//
+
+const HTTP = {
+  HEADER: {
+    AMAZON: {
+      PARAMETERS_SECRETS_TOKEN: "X-Aws-Parameters-Secrets-Token",
+    },
+  },
+};
+
+const DEFAULT = {
+  PARAMETERS_SECRETS_EXTENSION_HTTP_PORT: 2773,
+};
+
+//
+//
+// Main
+//
+
+async function getSecret(name) {
+  log.trace.var({ getSecret: name });
+
+  if (!process.env.AWS_SESSION_TOKEN) {
+    throw new ConfigurationError("No AWS_SESSION_TOKEN available");
+  }
+  if (!name) {
+    throw new ConfigurationError("No secret name provided");
+  }
+
+  const headers = {
+    [HTTP.HEADER.AMAZON.PARAMETERS_SECRETS_TOKEN]:
+      process.env.AWS_SESSION_TOKEN,
+  };
+  const port =
+    process.env.PARAMETERS_SECRETS_EXTENSION_HTTP_PORT ||
+    DEFAULT.PARAMETERS_SECRETS_EXTENSION_HTTP_PORT;
+  const params = { secretId: name };
+  const endpoint = `http://localhost:${port}/secretsmanager/get`;
+  const response = await axios.get(endpoint, { headers, params });
+  return response.data.SecretString;
+}
+
+//
+//
+// Export
+//
+
+export default getSecret;
